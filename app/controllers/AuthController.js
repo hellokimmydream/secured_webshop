100fdb8704fe94e0dd7520fb09086f00fa4138d7
CamilleJulieRais <rais.camille@gmail.com>
2026-03-25 12:11:20 +0100

    feat(ajout sel)
    [35][WIP]

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -4,6 +4,9 @@ const db = require('../config/db');
 // bibliothèque de hachage de mdp
 const bcrypt = require('bcrypt');
 
+// ajoute le sel
+const SALT_ROUNDS = 12;
+
 module.exports = {
 
     // ----------------------------------------------------------
@@ -18,15 +21,22 @@ module.exports = {
             return res.status(400).json({ error: 'Email et mot de passe requis' });
         }
 
-        const query = `SELECT * FROM users WHERE email = '${email}' AND password = '${password}'`;
-
-        db.query(query, (err, results) => {
+        // il faut rechercher l utilisateur par email uniquemant
+        db.query('SELECT * FROM users WHERE email = ?', [email], async (err, results) => {
             if (err) {
-                return res.status(500).json({ error: err.message, query: query });
+                return res.status(500).json({ error: 'erreur serveur' });
             }
 
             if (results.length === 0) {
                 return res.status(401).json({ error: 'Email ou mot de passe incorrect' });
+            }
+
+            const user = results[0];
+
+            // compare le mdp entré avec le hash stocké dans la db
+            const isValid = await bcrypt.compare(password, user.password)
+            if (!isValid) {
+                return res.status(401).json({error: 'Erreur lors de la connexion'})
             }
             // pour ne jamais envoyer le mdp au client
             const {password: _pwd, ...safeUser} = user;
@@ -38,6 +48,31 @@ module.exports = {
     // POST /api/auth/register
     // ----------------------------------------------------------
     register: (_req, res) => {
-        res.status(501).json({ error: 'Non implémenté — TODO exercice 7' });
+        const {username, email, password} = req.body;
+
+        if(!username || !email || !password){
+            return res.status(400).json({ error: 'Tous les champs sont requis' });
+        }
+
+        try {
+            // On hashe le mot de passe avant de l'insérer en BDD
+            const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+            db.query(
+                'INSERT INTO users (username, email, password, role) VALUES (?, ?, ?, ?)',
+                [username, email, hashedPassword, 'user'],
+                (err, result) => {
+                    if (err) {
+                        if (err.code === 'ER_DUP_ENTRY') {
+                            return res.status(409).json({ error: 'Entrez un autre email' });
+                        }
+                        return res.status(500).json({ error: 'Erreur serveur' });
+                    }
+                    res.status(201).json({ message: 'Compte créé avec succès', userId: result.insertId });
+                }
+            );
+        } catch (err) {
+            res.status(500).json({ error: 'Erreur serveur' });
+        }
     }
 };
--- app/db/init/init.sql
+++ app/db/init/init.sql
@@ -40,8 +40,8 @@ CREATE TABLE IF NOT EXISTS products (
 -- Données de départ
 -- ---------------------------------------------------------------
 INSERT INTO users (username, email, password, role, address) VALUES
-    ('admin',  'admin@webshop.com', 'admin123',  'admin', '1 Rue de la Paix, 1000 Lausanne'),
-    ('alice',  'alice@webshop.com', 'password1', 'user',  '42 Avenue des Alpes, 1200 Genève');
+    ('admin',  'admin@webshop.com', '$2b$12$GSTGfXeshIGCRlkWUGPmEe5Wg6nN43/OkZXx4iFWlU1AR9hKOY8FK',  'admin', '1 Rue de la Paix, 1000 lausanne'),
+    ('alice',  'alice@webshop.com', '$2b$12$yb8ExwS62xYeXq7IgJernuus1CygB5h8kH.MxC99yQ59a8XP5yAti', 'user',  '42 Avenue des Alpes, 1200 Genève');
 
 INSERT INTO products (name, description, price, image_url) VALUES
     (
